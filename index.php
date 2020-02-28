a0e2aa331622ca1adfb4510d9dc3937202e1ac2f
Z3r0ish <victor.gualberto313@gmail.com>
2020-02-28 17:55:07 -0500

    php test

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php echo file_get_contents("html/header.html"); ?>
+<title>Bloc | Home</title>
+<meta property="og:url" content="https://bloc.gq">
+<?php echo file_get_contents("html/header2.html"); ?>
+<h1 class="center" style="padding-top:30px; font-size: 100px">The Bloc Project</h1>
+
+<div class="body">
+<p>The Bloc is a place of collaborative storytelling, drawing its playerbase from all different places in the world. We converge here in spirit to create a strange and immersive world of government, metaphysics and affliction. Here you will find a unique environment with things such as original slang and immersive settings, along with many more enriching features. The environment we create is sustained by our reactions to it, and our interactions with others within it. This allows creativity to flow in a variety of interesting ways, from insightful satire to true expressions of personal experience.
+<br><br>
+This environment is known as The Bloc, an infinite tower of dilapidated concrete, extending forever both upwards and downwards. The citizens within are a curious, typically Slavic type of humans that exist within a complex system of totalitarian government, known as Maintenance.
+<br><br>
+There is no outside.
+<br><br>
+It is forbidden both out of game and in-game to speak of anything related to a supposed "outside," this includes mythical structures known as "trees" or made up objects such as the "sun."
+<br><br>
+There is only The Bloc, and within The Bloc - Maintenance is in control.
+<br><br>
+Our characters range from scrappy junkyard Divers to ominous Maintenance Officers to ravenous babbling cultists, there is truly something for everyone here at The Bloc. Unlike other roleplay communities, the nature of The Bloc itself is both nebulous and forever-unknown. This allows for very creative character and world building, you will not find us to be too rigid or restrictive.
+<br><br>
+The Bloc is a place for everyone to shed their day and immerse themselves in a new world, a world where you can be truly yourself. There is a place for everybody in The Bloc.
+<br><br>
+Bloc to Prosper.
+</div>
+  </div>
+</div>
+<?php echo file_get_contents("html/footer.html"); ?>
---
+++ irc.php
@@ -0,0 +1,5 @@
+<?php echo file_get_contents("html/header.html"); ?>
+<title>Bloc | IRC</title>
+<meta property="og:url" content="https://bloc.gq/irc">
+<?php echo file_get_contents("html/header2.html"); ?>
+<iframe scrolling="no" border:"0" src="https://kiwiirc.com/client/irc.geekshed.net/?nick=bloc_citiz|?&theme=dark#bloc" style="border:0; width:100%; height:calc(100vh - 66px); position:absolute;"></iframe>
